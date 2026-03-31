e9f7aad2576a4cd4b9fce6f06ea8c7062076ce75
Dora-19 <doraalkan@yahoo.com>
2026-03-31 18:00:52 +0300

    Initial Commit

---
+++ C_project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_project
+//
+//  Created by Dora Alkan on 31.03.2026.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
